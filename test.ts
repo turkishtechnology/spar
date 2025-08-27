d2174980259cb957e60d8b1038572750f9a7b5c0
Harun Demir <harundemirp@gmail.com>
2025-08-27 09:15:06 +0300

    feat(husky): add husky, lint-staged, prettier, eslint configuration (#3)

---
+++ test.ts
@@ -0,0 +1,6 @@
+// TODO: lint'i test edebilmek için yazıldı. Daha sonra silinebilir.
+// Test file for ESLint configuration
+const testVariable = 'Hello World';
+console.log(testVariable);
+
+export default testVariable;
