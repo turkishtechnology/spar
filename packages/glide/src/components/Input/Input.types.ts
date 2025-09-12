a378c4fd239eb326c4f5c3d60e96f4b01e3bac42
Muhammet Can Tonbul <m.cantonbul@gmail.com>
2025-09-12 12:02:03 +0300

    refactor(components): remove button and input components for architecture cleanup

--- packages/glide/src/components/Input/Input.types.ts
+++
@@ -1,7 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
-  error?: string;
-  helperText?: string;
-}
