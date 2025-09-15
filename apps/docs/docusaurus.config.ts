20583438553d0c08378c524f1582558ea002fce2
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2025-09-15 14:37:13 +0300

    docs(style): Applied documentation design  (#23)
    * feat(docs): clear docusaurus default folders
    
    * feat(docs): add scss files
    
    * docs(ui): fix UI
    
    ---------
    
    Co-authored-by: Harun Demir <harundemirp@gmail.com>

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -1,13 +1,12 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Turkish Technology Headless UI',
+  tagline: 'Modern headless React component library',
+  favicon: 'img/Vector3.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,15 +14,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://headless-ui-docs.turkishtechnology.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'turkishtechnology', // Usually your GitHub org/user name.
+  projectName: 'headless', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -52,35 +51,19 @@ const config: Config = {
       },
     },
   },
-
+  plugins: ['docusaurus-plugin-sass'],
   presets: [
     [
       'classic',
       {
         docs: {
+          path: 'docs',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: 'docs',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: require.resolve('./src/styles/index.scss'),
         },
       } satisfies Preset.Options,
     ],
@@ -90,74 +73,30 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      // title: "Takeoff UI",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        srcDark: 'img/thy-logo.svg',
+        src: 'img/thy-logo-dark.svg',
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        // { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: 'https://github.com/turkishtechnology/headless',
+          label: 'Github',
+          position: 'right',
+        },
         {
           type: 'localeDropdown',
           position: 'right',
         },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      hideOnScroll: true,
     },
     prism: {
       theme: prismThemes.github,
---
+++ apps/docs/src/components/LandingPage/Ending.tsx
@@ -0,0 +1,19 @@
+import Translate from '@docusaurus/Translate';
+
+export default function Ending() {
+  return (
+    <div className='ending-container'>
+      <div className='ending-texts'>
+        <div className='ending-title1'>
+          <Translate description='Are you ready to use the'>ending.ready.to.use</Translate>
+        </div>
+        <div className='ending-title2'>
+          <Translate description='headless Takeoff UI library?'>ending.headless.library</Translate>
+        </div>
+      </div>
+      <div>
+        <img src='img/ending-bg.png' />
+      </div>
+    </div>
+  );
+}
