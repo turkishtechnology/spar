daa9f8bffdb513f4092ea792487cfccd0c2a93b6
Kivanc Eski <eskikivanc@gmail.com>
2025-10-21 16:10:03 +0300

    refactor(interacthook): refactor interaction outside of a ref to a custom hook

--- packages/glide/src/hooks/index.ts
+++ packages/glide/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useFocusItem } from './useFocusItem';
 export { useControlledState } from './useControlledState';
 export { useMergedRef } from './useMergedRef';
+export { useInteractOutside, type UseInteractOutsideOptions } from './useInteractOutside';
