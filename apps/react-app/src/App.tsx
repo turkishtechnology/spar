149a444f5299d8a1fbff3ebacd5a500569ea37d0
Kivanc Eski <eskikivanc@gmail.com>
2026-01-13 22:35:29 +0300

    chore: add demo react app

---
+++ apps/react-app/src/App.tsx
@@ -0,0 +1,55 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionTrigger,
+  AccordionContent,
+  Button,
+  BreadcrumbRoot,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+  Dialog,
+  Input,
+  PopoverRoot,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverPortal,
+  PopoverAnchor,
+  PopoverClose,
+  Radio,
+  RadioGroup,
+  RadioItem,
+  Checkbox,
+  Switch,
+  Label,
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipPortal,
+  TooltipArrow,
+  Select,
+  DropdownMenu,
+} from '@turkish-technology/spar';
+
+function App() {
+  return (
+    <>
+      <div>Demo React App for Spar testing</div>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
