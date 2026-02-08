63784300e0f5cd05f8b89687083ea94816cb52ad
Harun Demir <harundemirp@gmail.com>
2026-02-08 19:36:27 +0300

    refactor(radio): remove unnecessary Radio.tsx file

--- packages/spar/src/components/Radio/Radio.tsx
+++
@@ -1,9 +0,0 @@
-import { RadioGroup } from './RadioGroup';
-import { RadioItem } from './RadioItem';
-
-/**
- * Alias for RadioGroup to preserve the base component export.
- */
-export const Radio = RadioGroup;
-
-export { RadioGroup, RadioItem };
