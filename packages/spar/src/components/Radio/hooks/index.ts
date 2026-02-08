bd4d744366aa928892e4da939a018d7f18bd4ac3
Harun Demir <harundemirp@gmail.com>
2026-02-08 18:28:11 +0300

    refactor(radio): consolidate context hooks into a single hooks module

---
+++ packages/spar/src/components/Radio/hooks/index.ts
@@ -0,0 +1,1 @@
+export { RadioGroupContext, useRadioGroupContext } from './useRadioGroupContext';
--- packages/spar/src/components/Radio/index.ts
+++ packages/spar/src/components/Radio/index.ts
@@ -2,5 +2,6 @@ export { Radio } from './Radio';
 export { RadioGroup } from './RadioGroup';
 export { RadioItem } from './RadioItem';
 export { RadioGroup as RadioRoot } from './RadioGroup';
+export { useRadioGroupContext } from './hooks';
 
 export type { RadioGroupProps, RadioItemProps, RadioItemRenderProps } from './types';
