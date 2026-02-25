ec2e40b08acfb7b303a988d7a7386c7f78dd6dfd
Kivanc Eski <eskikivanc@gmail.com>
2026-02-25 09:29:02 +0300

    refactor: remove redundant dropdownmenu subcomponents

--- packages/spar/src/components/DropdownMenu/index.ts
+++ packages/spar/src/components/DropdownMenu/index.ts
@@ -2,52 +2,28 @@ import { DropdownMenu as DropdownMenuRoot } from './DropdownMenu';
 import { DropdownMenuTrigger } from './DropdownMenuTrigger';
 import { DropdownMenuContent } from './DropdownMenuContent';
 import { DropdownMenuItem } from './DropdownMenuItem';
-import { DropdownMenuCheckboxItem } from './DropdownMenuCheckboxItem';
-import { DropdownMenuRadioGroup } from './DropdownMenuRadioGroup';
-import { DropdownMenuRadioItem } from './DropdownMenuRadioItem';
 import { DropdownMenuSeparator } from './DropdownMenuSeparator';
 import { DropdownMenuLabel } from './DropdownMenuLabel';
 import { DropdownMenuGroup } from './DropdownMenuGroup';
-import { DropdownMenuSub } from './DropdownMenuSub';
-import { DropdownMenuSubTrigger } from './DropdownMenuSubTrigger';
-import { DropdownMenuSubContent } from './DropdownMenuSubContent';
-export {
-  useDropdownMenuContext,
-  useDropdownMenuSubContext,
-  useMenuScope,
-  useDropdownMenuRadioGroupContext,
-  useDropdownMenuCollectionContext,
-} from './hooks';
+export { useDropdownMenuContext, useDropdownMenuCollectionContext } from './hooks';
 
 const DropdownMenu = DropdownMenuRoot as typeof DropdownMenuRoot & {
   Root: typeof DropdownMenuRoot;
   Trigger: typeof DropdownMenuTrigger;
   Content: typeof DropdownMenuContent;
   Item: typeof DropdownMenuItem;
-  CheckboxItem: typeof DropdownMenuCheckboxItem;
-  RadioGroup: typeof DropdownMenuRadioGroup;
-  RadioItem: typeof DropdownMenuRadioItem;
   Separator: typeof DropdownMenuSeparator;
   Label: typeof DropdownMenuLabel;
   Group: typeof DropdownMenuGroup;
-  Sub: typeof DropdownMenuSub;
-  SubTrigger: typeof DropdownMenuSubTrigger;
-  SubContent: typeof DropdownMenuSubContent;
 };
 
 DropdownMenu.Root = DropdownMenuRoot;
 DropdownMenu.Trigger = DropdownMenuTrigger;
 DropdownMenu.Content = DropdownMenuContent;
 DropdownMenu.Item = DropdownMenuItem;
-DropdownMenu.CheckboxItem = DropdownMenuCheckboxItem;
-DropdownMenu.RadioGroup = DropdownMenuRadioGroup;
-DropdownMenu.RadioItem = DropdownMenuRadioItem;
 DropdownMenu.Separator = DropdownMenuSeparator;
 DropdownMenu.Label = DropdownMenuLabel;
 DropdownMenu.Group = DropdownMenuGroup;
-DropdownMenu.Sub = DropdownMenuSub;
-DropdownMenu.SubTrigger = DropdownMenuSubTrigger;
-DropdownMenu.SubContent = DropdownMenuSubContent;
 
 export {
   DropdownMenu,
@@ -55,15 +31,9 @@ export {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
   DropdownMenuSeparator,
   DropdownMenuLabel,
   DropdownMenuGroup,
-  DropdownMenuSub,
-  DropdownMenuSubTrigger,
-  DropdownMenuSubContent,
 };
 
 export type {
@@ -72,13 +42,7 @@ export type {
   DropdownMenuTriggerRenderProps,
   DropdownMenuContentProps,
   DropdownMenuItemProps,
-  DropdownMenuCheckboxItemProps,
-  DropdownMenuRadioGroupProps,
-  DropdownMenuRadioItemProps,
   DropdownMenuSeparatorProps,
   DropdownMenuLabelProps,
   DropdownMenuGroupProps,
-  DropdownMenuSubProps,
-  DropdownMenuSubTriggerProps,
-  DropdownMenuSubContentProps,
 } from './types';
