6cd4a4af06006f9cc1c0f038073d2be6c201be1b
denizhanyigit <denizhan.yigit@hotmail.com>
2026-03-04 22:52:06 +0300

    refactor: extract close button logic into reusable hook

--- packages/spar/src/hooks/index.ts
+++ packages/spar/src/hooks/index.ts
@@ -5,3 +5,4 @@ export { useMergedRef } from './useMergedRef';
 export { useInteractOutside, type UseInteractOutsideOptions } from './useInteractOutside';
 export { useItemRegistry } from './useItemRegistry';
 export type { ItemRegistry } from './useItemRegistry';
+export { useCloseButton, type CloseButtonRenderProps } from './useCloseButton';
