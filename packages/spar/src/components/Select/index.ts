66db3cd431ac56216f089219d9ed05701dc4aa0f
Ulaş Turan <ulasturan06@hotmail.com>
2026-03-07 19:35:58 +0300

    refactor(select): remove select icon and item indicator components and docs references

--- packages/spar/src/components/Select/index.ts
+++ packages/spar/src/components/Select/index.ts
@@ -1,13 +1,13 @@
 import { Select as SelectRoot } from './Select';
 import { SelectTrigger } from './SelectTrigger';
 import { SelectValue } from './SelectValue';
-import { SelectIcon } from './SelectIcon';
+
 import { SelectContent } from './SelectContent';
 import { SelectItem } from './SelectItem';
 import { SelectGroup } from './SelectGroup';
 import { SelectLabel } from './SelectLabel';
 import { SelectItemText } from './SelectItemText';
-import { SelectItemIndicator } from './SelectItemIndicator';
+
 import { SelectSeparator } from './SelectSeparator';
 import { SelectArrow } from './SelectArrow';
 export {
@@ -21,13 +21,13 @@ const Select = SelectRoot as typeof SelectRoot & {
   Root: typeof SelectRoot;
   Trigger: typeof SelectTrigger;
   Value: typeof SelectValue;
-  Icon: typeof SelectIcon;
+
   Content: typeof SelectContent;
   Item: typeof SelectItem;
   Group: typeof SelectGroup;
   Label: typeof SelectLabel;
   ItemText: typeof SelectItemText;
-  ItemIndicator: typeof SelectItemIndicator;
+
   Separator: typeof SelectSeparator;
   Arrow: typeof SelectArrow;
 };
@@ -35,13 +35,13 @@ const Select = SelectRoot as typeof SelectRoot & {
 Select.Root = SelectRoot;
 Select.Trigger = SelectTrigger;
 Select.Value = SelectValue;
-Select.Icon = SelectIcon;
+
 Select.Content = SelectContent;
 Select.Item = SelectItem;
 Select.Group = SelectGroup;
 Select.Label = SelectLabel;
 Select.ItemText = SelectItemText;
-Select.ItemIndicator = SelectItemIndicator;
+
 Select.Separator = SelectSeparator;
 Select.Arrow = SelectArrow;
 
@@ -50,13 +50,11 @@ export {
   SelectRoot,
   SelectTrigger,
   SelectValue,
-  SelectIcon,
   SelectContent,
   SelectItem,
   SelectGroup,
   SelectLabel,
   SelectItemText,
-  SelectItemIndicator,
   SelectSeparator,
   SelectArrow,
 };
@@ -66,14 +64,12 @@ export type {
   SelectTriggerProps,
   SelectTriggerRenderProps,
   SelectValueProps,
-  SelectIconProps,
   SelectContentProps,
   SelectItemProps,
   SelectItemRenderProps,
   SelectGroupProps,
   SelectLabelProps,
   SelectItemTextProps,
-  SelectItemIndicatorProps,
   SelectSeparatorProps,
   SelectArrowProps,
   SelectContextValue,
