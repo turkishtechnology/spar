3a519904c6ba2ff73744709c26261576693ce19f
Harun Demir <harundemirp@gmail.com>
2026-03-07 18:24:16 +0300

    refactor(arrows): add createArrowComponent hook

--- packages/spar/src/utils/index.ts
+++ packages/spar/src/utils/index.ts
@@ -1,2 +1,8 @@
 export { visuallyHidden, getVisuallyHiddenStyles } from './visuallyHidden';
 export { getPlacement } from './getPlacement';
+export { createArrowComponent } from './createArrowComponent';
+export type {
+  FloatingArrowProps,
+  ArrowRootContext,
+  ArrowContentContext,
+} from './createArrowComponent';
