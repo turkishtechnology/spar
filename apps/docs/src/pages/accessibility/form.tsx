bea349db0090856b5c59e023be21c5de8e3aa26d
Pınar Yalçınduran <104770005+pinaryalcinduran@users.noreply.github.com>
2026-04-07 15:48:35 +0300

    fix: enhance accessibility by updating aria attributes in Radio components

--- apps/docs/src/pages/accessibility/form.tsx
+++ apps/docs/src/pages/accessibility/form.tsx
@@ -226,13 +226,13 @@ export default function FormDemo() {
                         <Radio.Item
                           className='demo-radio-item'
                           value={option.value}
-                          id={`plan-${option.value}`}
+                          aria-labelledby={`plan-${option.value}`}
                         >
                           {({ isChecked }) =>
                             isChecked ? <span className='demo-radio-indicator' /> : null
                           }
                         </Radio.Item>
-                        <Label htmlFor={`plan-${option.value}`} className='demo-label'>
+                        <Label id={`plan-${option.value}`} className='demo-label'>
                           {option.label}
                         </Label>
                       </div>
